a170639a8fadefe67a0473d9b925a26825cae71b
hilxx <eunjung377@naver.com>
2023-07-10 12:41:53 +0900

    2weeks challenge start

---
+++ MOMENTUM/app.js
@@ -0,0 +1,10 @@
+const a = 5;
+const b = 2;
+
+const myName = "hilee";
+//pyson에서는 myName = "hilee" 이렇게만 쓰면 됨
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
