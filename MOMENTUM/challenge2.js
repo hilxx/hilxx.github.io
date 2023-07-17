a62fbfa97c1ae390dbc24b254e324750267dd40e
hilxx <eunjung377@naver.com>
2023-07-17 23:23:00 +0900

    230717_ch7일차_className완료

--- MOMENTUM/challenge2.js
+++ MOMENTUM/challenge2.js
@@ -3,16 +3,19 @@ const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
     const clickedClass = "clicked";
-    // h1.className = "active";
-    //  if(h1.className === clickedClass){}
-    if(h1.className.contain(clickedClass) === clickedClass) {
-        // h1.className = "";
-        h1.classList.remove(clickedClass);
-    } else{
-        // h1.className = clickedClass;
-        h1.classList.add(clickedClass);
-    }
-    console.log(h1.className);
+    // 1. h1.className = "active";
+    // 2. if(h1.className === clickedClass){}
+    // 3. if(h1.className.contain(clickedClass) === clickedClass) {
+    //     2,3.// h1.className = "";
+    //     4. h1.classList.remove(clickedClass);
+    // } else{
+    //     2,3.// h1.className = clickedClass;
+    //     4. h1.classList.add(clickedClass);
+    // }
+    // console.log(h1.className);
+
+    //5. h1.classList.toggle(clickedClass);
+    h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleClick);
